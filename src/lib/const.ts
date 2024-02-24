e68ce1ac0af8b6d5adb23db1200a73e43b95da61
Nguyen Tung Lam <nguyentunglam4w@gmail.com>
2024-02-24 17:33:43 +0700

    update: retructure components folder

---
+++ src/lib/const.ts
@@ -0,0 +1,66 @@
+export const markdownSyntax = [
+  {
+    ele: "Heading 1",
+    syntax: "#",
+    caret: 1,
+  },
+  {
+    ele: "Heading 2",
+    syntax: "##",
+    caret: 1,
+  },
+  {
+    ele: "Heading 3",
+    syntax: "###",
+    caret: 3,
+  },
+  {
+    ele: "Bold",
+    syntax: "****",
+    caret: 1,
+  },
+  {
+    ele: "Italic",
+    syntax: "**",
+    caret: 0,
+  },
+  {
+    ele: "Blockquote",
+    syntax: ">",
+    caret: 1,
+  },
+  {
+    ele: "Unordered List",
+    syntax: "-",
+    caret: 2,
+  },
+  {
+    ele: "Divider",
+    syntax: "---",
+    caret: 3,
+  },
+];
+
+export const designTemplate = [
+  {
+    template: "Indius",
+  },
+  {
+    template: "Vega",
+  },
+];
+
+export const defaultColors = [
+  "#E8384F",
+  "#E74C3C",
+  "#E67E22",
+  "#F1C40F",
+  "#A4CF30",
+  "#2ECC71", 
+  "#1ABC9C",
+  "#016EF1",
+  "#7A6FF0",
+  "#E362E3",
+  "#8DA3A6",
+  "#222222",
+]
