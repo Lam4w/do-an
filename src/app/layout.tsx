517f9c34f81850f3cfd398114fb09a563d5a2c45
garovu <garovu.dev@gmail.com>
2024-02-25 19:58:27 +0700

    update: change .css file to /styles directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Providers from "@/components/providers/Provider";
 import { Toaster } from "@/components/ui/Toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
